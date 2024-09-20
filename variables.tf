1893a767748d7fbc9b720b66fb0712d001db0252
Christine Robinson <robinson@energetics.ai>
2024-09-20 14:20:29 +0530

    Base setup updated

--- variables.tf
+++ variables.tf
@@ -3,9 +3,3 @@ variable "REGION" {
   default     = "ap-south-1"
   description = "AWS Region"
 }
-
-variable "S3_BUCKETS" {
-  type        = set(string)
-  default     = ["zen-devops-bucket-1", "zen-devops-bucket-2"]
-  description = "S3 Buckets"
-}
