09feeadc4dce12234e48321025c27f768257cfe9
Christine Robinson S <christinerobinson.dev@gmail.com>
2024-12-20 13:13:17 +0530

    initial setup

--- main.tf
+++ main.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.67.0"
+      version = "5.82.1"
     }
   }
 
   backend "s3" {
-    bucket         = "terraform-iac-lab"
+    bucket         = "terraform-iac-lab-backend"
     key            = "dev.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-iac-lab"
-    encrypt        = false
+    dynamodb_table = "terraform-iac-lab-backend-lock"
+    encrypt        = true
   }
 }
 
 provider "aws" {
-  region = var.REGION
+  region = var.AWS_DEFAULT_REGION
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
-variable "REGION" {
+variable "PROJECT_NAME" {
+  type        = string
+  default     = "terraform-iac-lab"
+  description = "Project Name"
+}
+
+variable "ENVIRONMENT" {
+  type        = string
+  default     = "dev"
+  description = "Environment"
+}
+
+variable "AWS_DEFAULT_REGION" {
   type        = string
   default     = "ap-south-1"
-  description = "AWS Region"
+  description = "AWS Default Region"
 }
